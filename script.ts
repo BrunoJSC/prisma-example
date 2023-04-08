b12e0dc0facaa1104304efd8a16e0d7334045e2c
Bruno de Jesus <bruno.jesus.carmo@gmail.com>
2023-04-08 12:24:18 -0300

    first commit

---
+++ script.ts
@@ -0,0 +1,49 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient()
+
+async function main() {
+  // const user = await prisma.user.create({
+  //   data: {
+  //     name: "Alice",
+  //     email: "alice@prisma.io"
+  //   }
+  // })
+
+  // console.log(user);
+  // return user;
+
+  // const users = await prisma.user.findMany();
+  // console.log(users);
+
+  // const user = await prisma.user.create({
+  //   data: {
+  //     name: "Bob",
+  //     email: "bob@prisma.io",
+  //     posts: {
+  //       create: {
+  //         title: "Hello World",
+  //         content: "I am a content"
+  //       }
+  //     }
+  //   }
+  // })
+  // console.log(user);
+
+    // include posts
+    const usersWithPosts = await prisma.user.findMany({
+      include: {
+        posts: true
+      }
+    })
+  
+    console.log(usersWithPosts, {depth: null});
+}
+
+main().then(async () => {
+  await prisma.$disconnect()
+}).catch(async(e) => {
+  console.error(e)
+  await prisma.$disconnect()
+  process.exit(1)
+})
